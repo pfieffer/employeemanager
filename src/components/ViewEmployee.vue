d497332a944a61315be96df5b48b370a08ffd18e
pfieffer <cbpmgr@gmail.com>
2019-02-16 02:14:53 +0545

    Setup basic views for our app. Working commit [x]

---
+++ src/components/ViewEmployee.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="view-employee">
+        <h3>View Empoyee</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'view-employee',
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
